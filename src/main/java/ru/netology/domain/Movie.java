bcd0a0bcedae9d4ec6e255c8cb383ec1a236f814
Vlad <Crash-vd@yandex.ru>
2022-11-30 20:36:55 +0300

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Первая версия проекта

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+
+public class Movie {
+    private int id;
+    private String name;
+    private String genre;
+    private boolean premiereTomorrow;
+
+    public Movie(int i, String бладшот, String боевик, boolean b) {
+    }
+}
